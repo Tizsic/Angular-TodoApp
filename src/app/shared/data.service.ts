4e1b2864ab521fa34b521c6a1824fe46d6ec235e
Stefan <stefan@hawser.org>
2022-09-02 11:11:39 -0400

    changes
    Added ng forms.
    added methods

---
+++ src/app/shared/data.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { Todo } from './todo.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  todos: Todo[] = [
+    new Todo('hey', false),
+    new Todo('this is a test', true)
+  ]
+
+  constructor() { }
+
+  getAllTodos () {
+    return this.todos
+  }
+
+  addTodo(todo: Todo) {
+    this.todos.push(todo)
+  }
+
+  updateTodo(index: number, updatedTodo: Todo) {
+    this.todos[index] = updatedTodo;
+  }
+
+  deleteTodo(index: number) {
+    this.todos.splice(index, 1)
+  }
+
+}
+
+
+
--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { DataService } from '../shared/data.service';
+import { Todo } from '../shared/todo.model';
 
 @Component({
   selector: 'app-todos',
@@ -7,9 +10,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodosComponent implements OnInit {
 
-  constructor() { }
+  todos: Todo[]
+
+  constructor(private dataService: DataService) { }
 
   ngOnInit(): void {
+    this.todos = this.dataService.getAllTodos();
+  }
+
+  onFormSubmit(form: NgForm){
+    console.log("Form Submitted");
   }
 
 }
